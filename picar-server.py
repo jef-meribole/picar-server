3d40199af26c383fa42d2f479fface8ebd3307f7
jmeribole <jmeribole@gmail.com>
2023-11-27 23:31:38 -0600

    execute arbitrary 'actions' from client

--- picar-server.py
+++ picar-server.py
@@ -1,33 +1,58 @@
 from socket import *
 
-ACTIONS = ["FORWARD", "BACKWARD", "LEFT", "RIGHT"]
 
-def do_action(action, unit):
-    pass
+def init_actions():
+    actions = {
+        "forward": move_car,
+        "backward": move_car,
+        "left": move_car,
+        "right": move_car,
+    }
+
+    return actions
 
 
-def parse_action(client_command: str) -> None:
-    action = client_command.split(":")[0]
-    unit = int(client_command.split(":")[1])
-    valid_action = action in ACTIONS;
+def do_action(action, unit) -> None:
+    action = action.lower()
+    actions = init_actions()
+    valid_action = action in actions.keys()
 
-    # Quit if action is not valid
     if not valid_action:
-        return;
+        print("INVALID ACTION SENT")
+        return
+
+    # Execute action with given unit
+    actions[action](unit)
+
+
+def move_car(unit: int) -> None:
+    print(f"ACTION: {unit}")
+
+
+def attempt_command(client_command: str) -> None:
+    action_args = client_command.split(":")
+
+    if len(action_args) != 2:
+        print("malformed action")
+        return
+
+    action = action_args[0]
+    unit = action_args[1]
 
     do_action(action, unit)
     return
 
 
 def main():
-    mySock = socket(AF_INET,SOCK_DGRAM)
-    mySock.bind(('', 12000))
+    mySock = socket(AF_INET, SOCK_DGRAM)
+    mySock.bind(("", 12000))
 
     while True:
         message, clientAddress = mySock.recvfrom(2048)
         message = message.decode()
-        parse_action(message)
+        attempt_command(message)
         mySock.sendto("got it".encode(), clientAddress)
+
 
 if __name__ == "__main__":
     main()
