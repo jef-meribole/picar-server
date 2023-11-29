868bf0c303be45620366e0d623a1320dd87fa1f9
jmeribole <jmeribole@gmail.com>
2023-11-28 22:03:12 -0600

    my brain clearly isn't working properly

--- picar-server.py
+++ picar-server.py
@@ -74,9 +74,8 @@ def main():
         command, clientAddress = mySock.recvfrom(2048)
         CURRENT_ID += 1
 
-        command = f"{command.decode()}:{CURRENT_ID}"
         do_action(command, CURRENT_ID)
-        print(command)
+        print(make_command(command, CURRENT_ID))
 
         mySock.sendto("got it".encode(), clientAddress)
 
