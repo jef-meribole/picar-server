e78ba078b1933b78c754e28580ed6216c2e2637b
picar2 <none>
2023-11-28 22:06:00 -0600

    Fix decode

--- picar-server.py
+++ picar-server.py
@@ -74,6 +74,8 @@ def main():
         command, clientAddress = mySock.recvfrom(2048)
         CURRENT_ID += 1
 
+        print(command)
+        command = command.decode()
         do_action(command, CURRENT_ID)
         print(make_command(command, CURRENT_ID))
 
