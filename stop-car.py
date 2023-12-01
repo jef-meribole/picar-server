944bfd9f0ffd2d4e45abb4753e9d380e96ab75dd
jmeribole <jmeribole@gmail.com>
2023-11-30 21:31:29 -0600

    clean up this file

--- stop-car.py
+++ stop-car.py
@@ -1,8 +1,9 @@
-from picarx import Picarx
+from picarx import Picarx # pylint: disable=import-error
 
 def main():
     picar = Picarx()
     picar.forward(0)
+
 
 if __name__ == "__main__":
     main()
